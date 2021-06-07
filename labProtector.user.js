66b876e97b736442467ced277185a85023627e60
kreotsai <43966453+kreotsai@users.noreply.github.com>
2021-06-06 21:56:53 -0400

    Update labProtector.user.js

--- labProtector.user.js
+++ labProtector.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NPC Lab Protector
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Removes pets from the lab page you don't want to accidentally zap!
 // @author       plushies
 // @include      https://neopetsclassic.com/lab2/
@@ -19,7 +19,13 @@
 //Put any pet names that you want to prevent from showing at the lab into the brackets below!
     
 //Names must be in the [] brackets, in "" quotes, with a comma separating each one.
-var hiddenPets = ["PETNAMEHERE", "PETNAMEHERE"]
+var hiddenPets = ["PETNAMEHERE", "PETNAMEHERE", "PETNAMEHERE"]
+
+//If you have a lot of pets, check the console log for a pre-formatted list of all of your pets' names!
+//Copy and paste that in the brackets, and remove the ones you don't want to hide :)
+
+
+var allPets = []
 
 var divs = document.getElementsByTagName('div');
 
@@ -30,6 +36,7 @@ var divs = document.getElementsByTagName('div');
         if(div.style.cssText.indexOf("float: left; position: relative;") !== -1)
         {
             var petName = div.getElementsByTagName('img')[0].getAttribute("alt");
+            allPets += '"' + petName + '", ';
 
              for (var p = 0; p < hiddenPets.length; p++)
              {
@@ -37,15 +44,18 @@ var divs = document.getElementsByTagName('div');
                  {
                      console.log("Hiding " + petName + "!");
                      div.style.display = "none";
+
                  }
              }
 
 
 
-            
+           
 
         }
 
     }
+console.log("Logging all pets for easy copy & paste into the script:");
+console.log(allPets);
 
 })();
